2ad6f9e559656d114f0dd6b03d0f953b97575cf8
Roman <rb@buechler.pro>
2021-03-01 10:36:53 +0200

    [FEATURE] Add configuration to force copying configuration

--- Classes/Slot/ExtensionInstallUtility.php
+++ Classes/Slot/ExtensionInstallUtility.php
@@ -11,6 +11,8 @@ namespace Buepro\UserPizpalue\Slot;
 
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ExtensionInstallUtility
@@ -31,27 +33,52 @@ class ExtensionInstallUtility
         // Add default `AdditionalConfiguration.php`
         $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
         if ($extensionConfiguration->get('user_pizpalue', 'addAdditionalConfiguration')) {
-            $this->copyAdditionalConfiguration();
+            $force = (bool) $extensionConfiguration->get('user_pizpalue', 'forceAdditionalConfiguration');
+            $this->copyAdditionalConfiguration($force);
         }
     }
 
     /**
-     * Copies the default AdditionalConfiguration file.
+     * Copies the default AdditionalConfiguration file. If forced to copy the previous file is copied to
+     * `AdditionalConfiguration_[timeStamp].backup`
      *
+     * @param bool $force
      * @return bool returns true if the default file has been copied
      */
-    private function copyAdditionalConfiguration()
+    private function copyAdditionalConfiguration($force = false): bool
     {
         $source = Environment::getPublicPath()
-            . '/typo3conf/ext/user_pizpalue/Resources/Private/FolderStructureTemplateFiles/AdditionalConfiguration.php';
+            . '/typo3conf/ext/user_pizpalue/Resources/Private/ComposerRoot/public/typo3conf/AdditionalConfiguration.php';
         $destination = Environment::getPublicPath() . '/typo3conf/AdditionalConfiguration.php';
+        $backup = sprintf(
+            '%s_%d.backup',
+            Environment::getPublicPath() . '/typo3conf/AdditionalConfiguration',
+            time()
+        );
+        // There is nothing to copy...
         if (!file_exists($source)) {
             return false;
         }
-        if (!file_exists($destination)) {
-            return GeneralUtility::upload_copy_move($source, $destination);
+        // File already exists and we aren't allowed to overwrite it
+        if (!$force && file_exists($destination)) {
+            return false;
         }
-
-        return false;
+        // Copy, but backup if file already exists
+        if (file_exists($destination)) {
+            GeneralUtility::upload_copy_move($destination, $backup);
+        }
+        GeneralUtility::upload_copy_move($source, $destination);
+        // Notify
+        $message = GeneralUtility::makeInstance(FlashMessage::class,
+            $GLOBALS['LANG']->sL('LLL:EXT:user_pizpalue/Resources/Private/Language/Backend.xlf:ext_conf.additionalConfigurationChanged'),
+            '',
+            FlashMessage::NOTICE,
+            true
+        );
+        $flashMessageService = GeneralUtility::makeInstance(FlashMessageService::class);
+        $messageQueue = $flashMessageService->getMessageQueueByIdentifier('extbase.flashmessages.tx_extensionmanager_tools_extensionmanagerextensionmanager');
+        $messageQueue->addMessage($message);
+        // @todo Confirm copying went correctly
+        return true;
     }
 }
