9a608513e2d1c1119a63398a7cc6fcf153e20f79
Roman Büchler <rb@buechler.pro>
2022-11-15 13:36:05 +0100

    [TASK] Bump pizpalue and pizpalue_distribution

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-99.99.99',
-            'pizpalue' => '14.0.0-99.99.99',
+            'pizpalue' => '14.1.0-99.99.99',
         ],
         'conflicts' => [
         ],
