ec677758fd0c359c323f5456f59833f3b3bef42d
Roman Büchler <rb@buechler.pro>
2024-01-08 19:30:48 +0200

    [TASK] Add main menu dropdown js example
    Shows dropdowns when hovering over main menu
    entries.

--- Resources/Public/JavaScript/Src/general.js
+++ Resources/Public/JavaScript/Src/general.js
@@ -1,1 +1,15 @@
 // See as well EXT:pizpalue/Resources/Public/JavaScript/Src/pizpalue.utility.js
+
+/**
+ * Show main menu dropdowns when hovering over the main menu entries.
+ */
+// pp.domReady(function () {
+//     const hoverDropdownElements = document.querySelectorAll('#mainnavigation button.nav-link-toggle');
+//     hoverDropdownElements.forEach(dropdownElement => {
+//         const link = dropdownElement.parentElement.querySelector('a');
+//         link.addEventListener('mouseenter', evt => {
+//             hoverDropdownElements.forEach(possibleActiveDropdown => (new bootstrap.Dropdown(possibleActiveDropdown)).hide());
+//             (new bootstrap.Dropdown(dropdownElement)).show();
+//         });
+//     })
+// });
