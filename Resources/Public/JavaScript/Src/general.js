a69e4b57b364f57d6627cd0ff4ae0cec90616a4e
Roman Büchler <rb@buechler.pro>
2024-05-14 16:01:30 +0300

    [BUGFIX] Delay menu dropdown mouse event

--- Resources/Public/JavaScript/Src/general.js
+++ Resources/Public/JavaScript/Src/general.js
@@ -4,12 +4,14 @@
  * Show main menu dropdowns when hovering over the main menu entries.
  */
 // pp.domReady(function () {
-//     const hoverDropdownElements = document.querySelectorAll('#mainnavigation button.nav-link-toggle');
-//     hoverDropdownElements.forEach(dropdownElement => {
-//         const link = dropdownElement.parentElement.querySelector('a');
-//         link.addEventListener('mouseenter', evt => {
-//             hoverDropdownElements.forEach(possibleActiveDropdown => (new bootstrap.Dropdown(possibleActiveDropdown)).hide());
-//             (new bootstrap.Dropdown(dropdownElement)).show();
+//     setTimeout(() => {
+//         const hoverDropdownElements = document.querySelectorAll('#mainnavigation button.nav-link-toggle');
+//         hoverDropdownElements.forEach(dropdownElement => {
+//             const link = dropdownElement.parentElement.querySelector('a');
+//             link.addEventListener('mouseenter', evt => {
+//                 hoverDropdownElements.forEach(possibleActiveDropdown => (new bootstrap.Dropdown(possibleActiveDropdown)).hide());
+//                 (new bootstrap.Dropdown(dropdownElement)).show();
+//             });
 //         });
-//     })
+//    }, 100);
 // });
